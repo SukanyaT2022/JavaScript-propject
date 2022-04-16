7e26af9921a65314971aa9a1acf10134025b2e31
Tiparpron Sukanya <tiparpron@Tiparprons-Air.home>
2022-04-15 20:34:42 -0400

    continue

--- Color-Flipper/app.js
+++ Color-Flipper/app.js
@@ -1,14 +1,25 @@
 //const --fix value that can not be changed--NO CHANGE
 //let--use let for variable that can be changed
-//var--
+
 
 const colors = ["green", "red", "rgba(133,122,200)","#f15025" ]
 const btn = document.getElementById("btn")// selecting btn which is id 
 const color = document.querySelector(".color")
 
+btn.addEventListener("click",function (){
+const randomNumber = getRandomNumber()
+//console.log(randomNumber) --we put console for check if code work properly
+document.body.style.backgroundColor = colors[randomNumber]// document.body captures colors on array--target background color
+color.textContent = colors[randomNumber]//target color class-it chnage value based on array color position
+})
+
+
+
+
+
 
 function getRandomNumber(){
-return Math.floor( math.random() * colors.length)// floor is the limit ex floor is  10 means 10.4 still ok but number from 10.5 and above is not ok that will give 11
+return Math.floor( Math.random() * colors.length)// floor is the limit ex floor is  10 means 10.4 still ok but number from 10.5 and above is not ok that will give 11
 
 }
 
